71d2bd9373df742871fc51442fc336c95f5582a8
Fernando Sobreira <fbsobreira@gmail.com>
2020-04-20 17:26:57 -0300

    ...

--- chewie-back/src/app/controllers/MeetingRoomController.js
+++ chewie-back/src/app/controllers/MeetingRoomController.js
@@ -11,7 +11,7 @@ class MeetingRoomController {
         const rooms = await MeetingRoom.findAll({
           limit: 20,
           offset: (page - 1) * 20,
-          attributes: [ 'id', 'name', 'room'],
+          attributes: [ 'id', 'name', 'room', 'description'],
           include: [{
               model: RoomItem,
               attributes: [ 'quantity'],
