c9cf20a8148f75ce7b7c550b846d17c18061ecda
deborahbaltasar <deborahbnogueira.dn@gmail.com>
2020-04-29 17:31:31 -0300

    update profile

--- chewie-front/src/pages/Profile/index.js
+++ chewie-front/src/pages/Profile/index.js
@@ -1,38 +1,112 @@
-import React from 'react';
-import {useHistory} from 'react-router-dom';
-import { Form, Input } from '@rocketseat/unform';
+import React, { Component } from 'react';
 
 import { toast } from 'react-toastify';
 
-import { Container } from './styles';
+import API from '../../services/api';
+
+import './styles.css';
 
 import AvatarInput from './AvatarInput';
 import Auth from '../../utils/auth';
 
-export default function Profile() {
-  const history = useHistory();
+class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      profile: [],
+      newName: '',
+      newEmail: '',
+      oldPass: '',
+      pass: '',
+      confirmPass: '',
+    }
+  }
 
-  return (
-    <Container>
-      <Form>
-        <AvatarInput name="avatar_id" />
-        <Input name="name" placeholder="Nome completo" />
-        <Input name="name" type="email" placeholder="Seu endereço de email" />
-      
-        <hr />
+  componentDidMount() {
+    this.fetchData()
+  }
 
-        <Input type="password" name="oldPassword" placeholder="Sua senha atual" />
-        <Input type="password" name="password" placeholder="Nova senha" />
-        <Input type="password" name="confirmPassword" placeholder="Confirmar senha atual" />
+  handleChange = e => {
+    this.setState({
+      newName: e.target.value,
+      newEmail: e.target.value,
+      oldPass: e.target.value,
+      pass: e.target.value,
+      confirmPass: e.target.value,
+    })
+  }
 
-        <button type="submit">Atualizar perfil</button>
-      </Form>
+  handleSubmit = async (e) => {
+    e.preventDefault();
 
-      <button onClick={()=>{
-        toast.warn("Bye!!!");
-        Auth.logOut();
-        history.push("/");
-      }} type="button">Sair do Chewie</button>
-    </Container>
-  );
+    const updateUser = {
+      name: this.state.newName,
+      email: this.state.newEmail,
+      oldPass: this.state.oldPass,
+      pass: this.state.pass,
+      confirmPass: this.state.confirmPass,
+    }
+
+    await API.put('/users', { updateUser })
+      .then(res => {
+        console.log("DADOS", res)
+        console.log(res.data)
+      })
+      .catch(error => {
+        console.log('Error ', error);
+        return { code: 'error', message: 'Cannot update!' };
+    });
+
+  }
+
+  fetchData = async (data) => {
+    await API.get('/sessions', {})
+      .then(res => {
+        this.setState({
+          name: res.data.name,
+          email: res.data.email,
+
+        });
+
+      })
+      .catch(error => {
+        console.log('Error ', error);
+        return { code: 'error', message: 'Cannot get user!' };
+      });
+  }
+
+
+  render() {
+    const { name, email } = this.state
+    const { history } = this.props;
+    return (
+      <div className="container">
+        <form >
+          <AvatarInput name="avatar_id" />
+          <input name="name" placeholder={name} />
+          <input name="name" type="email" placeholder={email} />
+
+          <hr />
+
+          <input type="password" name="oldPass" placeholder="Sua senha atual"  />
+          <input type="password" name="pass" placeholder="Nova senha"  />
+          <input type="password" name="confirmPass" placeholder="Confirmar senha atual"  />
+
+          <button className="update-profile-bnt" type="submit">Atualizar perfil</button>
+          <button className="quit-bnt" onClick={() => {
+            toast.warn("Bye!!!");
+            Auth.logOut();
+            history.push("/")
+          }} type="button">Sair do Chewie</button>
+
+        </form>
+
+
+      </div>
+    );
+  }
 }
+
+export default Profile;
