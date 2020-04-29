1e0207ef650b27371d07fe1efee5cd08eb28a5d4
deborahbaltasar <deborahbnogueira.dn@gmail.com>
2020-04-29 17:37:31 -0300

    update user

--- chewie-back/src/app/controllers/UserController.js
+++ chewie-back/src/app/controllers/UserController.js
@@ -1,19 +1,9 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
-    async index(req, res) {
-        const { page = 1 } = req.query;
-    
-        const users = await User.findAll({
-          limit: 20,
-          offset: (page - 1) * 20,
-          attributes: [ 'name', 'email'],
 
-        });
-        return res.json(users);
-      }
-    
     async store(req, res) {
         const schema = Yup.object().shape({
             name: Yup.string().required(),
@@ -45,44 +35,64 @@ class UserController {
         const schema = Yup.object().shape({
             name: Yup.string(),
             email: Yup.string().email(),
-            oldPassword: Yup.string().min(8),
-            password: Yup.string().min(8)
-                .when('oldPassword', (oldPassword, field) => 
-                    oldPassword ? field.required() : field,
-                ),
-            confirmPassword: Yup.string().when('password', (password, field) =>
+            oldPassword: Yup.string().min(6),
+            password: Yup.string()
+              .min(6)
+              .when('oldPassword', (oldPassword, field) =>
+                oldPassword ? field.required() : field
+              ),
+            confirmPassword: Yup.string()
+              .min(6)
+              .when('password', (password, field) =>
                 password ? field.required().oneOf([Yup.ref('password')]) : field
-            ),
-        });
-
-        if(!(await schema.isValid(req.body))) {
-            return res.status(400).json({ error: 'Validation fail'});
-        }
-
-        const { email, oldPassword } = req.body;
-
-        const user = await User.findByPk(req.userId);
-
-        if(email !== user.email) {
-            const userExists = await User.findOne({ where: {email} });
-
-            if(userExists) {
-                return res.status(400).json({ error: 'User already exists.' });
-            }   
-        }
-
-        if(oldPassword && !(await user.checkPassword(oldPassword))) {
-            return res.status(401).json({ error: 'Password does not match.' })
-        }
-
-        const {id, name, admin} = await user.update(req.body);
-        return res.json({
+              ),
+          });
+      
+          if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+          }
+      
+          const { email, oldPassword } = req.body;
+      
+          const user = await User.findByPk(req.userId);
+          if (email !== user.email) {
+            const userExists = await User.findOne({
+              where: {
+                email,
+              },
+            });
+            if (userExists) {
+              return res.status(400).json({
+                error: 'User already exists',
+              });
+            }
+          }
+      
+          if (oldPassword && !(await user.checkPassword(oldPassword))) {
+            return res.status(401).json({
+              error: 'Password does not match',
+            });
+          }
+      
+          await user.update(req.body);
+      
+          const { id, name, avatar } = await User.findByPk(req.userId, {
+            include: [
+              {
+                model: File,
+                as: 'avatar',
+                attributes: ['id', 'path', 'url'],
+              },
+            ],
+          });
+      
+          return res.json({
             id,
             name,
             email,
-            admin,
-        });
-    }
+            avatar,
+          });
+        }
 }
 
 export default new UserController();
