bdd7f224a29670db648987930c2b1ac328102872
deborahbaltasar <deborahbnogueira.dn@gmail.com>
2020-05-07 15:12:53 -0300

    delete meetings

--- chewie-back/src/app/controllers/MeetingController.js
+++ chewie-back/src/app/controllers/MeetingController.js
@@ -8,13 +8,12 @@ import Meeting from '../models/Meeting';
 class MeetingController {
   async index(req, res) {
     const { page = 1 } = req.query;
-    console.log("REQ", req.userId)
     const allMeetings = await Meeting.findAll({
       where: { canceled_at: null},
       order: ['start'],
       limit: 20,
       offset: (page - 1) * 20,
-      attributes: [ 'name', 'start', 'end'],
+      attributes: [ 'id', 'name', 'start', 'end'],
       include: [
         {
           model: User,
@@ -53,8 +52,10 @@ class MeetingController {
       .status(401)
       .json({ error: 'Meeting room does not exists' });
     }
-
+    const { id } = req.body;
+    
     const meeting = await Meeting.create({
+        id,
         name,
         user_id: req.userId,
         meeting_room_id,
@@ -66,7 +67,22 @@ class MeetingController {
   }
 
   async delete(req, res) {
-    return res.json();
-  }
+
+    const meeting = await Meeting.findByPk(req.params.id);
+
+    if (meeting.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You dont have permission to cancel this meeting"
+      })
+    }
+    
+    meeting.canceled_at = new Date();
+
+    await meeting.save();
+    
+    return res.json(meeting);
+
+  }  
+
 }
 export default new MeetingController();
