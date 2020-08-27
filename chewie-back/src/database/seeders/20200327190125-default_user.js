3aa4b190753eb4de4223463a21ef47d4c1ca4832
deborahbaltasar <deborahbnogueira.dn@gmail.com>
2020-08-27 09:50:07 -0300

    update users seed

--- chewie-back/src/database/seeders/20200327190125-default_user.js
+++ chewie-back/src/database/seeders/20200327190125-default_user.js
@@ -9,6 +9,9 @@ module.exports = {
         email: 'admin@unifor.br',
         password_hash: '$2a$08$1zhYwwN5mddrUIDdLlHU6O2tF2.UBQzLYdb6Pse./d7fosx/uxiZG',
         admin: true,
+        fisrt_logged_in: true,
+        avatar_id: null,
+        meeting_room_id: null,
         created_at: new Date(),
         updated_at: new Date()
       }
